190c966fdbfb08f390896a71c9c8883e51ae4194
Tim Hynes <Tim Hynes>
2016-12-28 19:56:16 +0000

    Updated shell script for Ubuntu MySQL install

--- bootstrap/mysql_install_ubuntu.sh
+++ bootstrap/mysql_install_ubuntu.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+MYSQL_PASS='VMware1!'
 apt-get update -y && apt-get upgrade -y
-debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password VMware1!'
-debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password_again password VMware1!'
+debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password $MYSQL_PASS'
+debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password_again password $MYSQL_PASS'
 apt-get install mysql-server -y
+MYSQL_SCRIPT="\n
+CREATE DATABASE sfapp;\n
+
+"
+mysql -uroot -p$MYSQL_PASS -e "$MYSQL_SCRIPT"
